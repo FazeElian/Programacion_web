b01f6cbf59b3cdba223894e9e136c468f4ff7bb5
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-05-18 17:30:02 -0500

    Condicionales if-else

---
+++ .php/Aprendizaje/Condicionales/if_else.php
@@ -0,0 +1,18 @@
+<!-- Condicionales  if y else -->
+<?php
+    echo "<h1> Condicionales If y Else </h1>";
+
+    //Creamos unas variables y les asignamos un valor:
+    $num1 = 10;
+    $num2 = 4;
+
+    //Para empezar una condición iniciarmos con if, que significa si(esto es igual a esto o si pasa esto)
+    if($num1 + $num2 = 14){ //Haz esto:
+        echo "Sí, el resultado de la suma es: 14";
+    }
+
+    //Y para terminar una condición se usa else, que significa sino o por el contrario, es decir se va a ejecutar cuando la primera condición if no se cumpla
+    else{
+        echo "No, el resultado de la suma es: 14";
+    }
+?> 
