ef388d2868d532b25bdcc1e060e4e18fd56a3218
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-05-22 11:16:13 -0500

    Arrays en PHP

--- .php/Aprendizaje/Arrays/Arrays.php
+++ .php/Aprendizaje/Arrays/Arrays.php
@@ -3,5 +3,5 @@
     echo $nombre[3]; //Imprimo cadena de texto de la variable nombre que está en la posición 3 = Valeria
 
     $nombre[1] = "Julian"; //Cambiamos la cadena de texto que está en la posición 1 por "Julian"
-    echo "" . $nombre[1];
+    echo "<br>" . $nombre[1];
 ?> 
