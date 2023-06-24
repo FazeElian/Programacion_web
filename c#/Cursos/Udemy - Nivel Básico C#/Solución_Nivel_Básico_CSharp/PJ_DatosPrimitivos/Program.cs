f7e843cb3c2912446a50ccce52e52c26ec02ea2e
FazeElian <elian.ibarra@soy.sena.edu.co>
2023-06-24 17:21:40 -0500

    Explicación gráfica entorno VS - Tipos de datos más usados en C#

---
+++ c#/Cursos/Udemy - Nivel Básico C#/Solución_Nivel_Básico_CSharp/PJ_DatosPrimitivos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DatosPrimitivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Definir tipo de dato de variable edad
+            int edad;
+            string nombre;
+            double sueldo;
+            bool casado;
+
+            edad = 14;
+            nombre = "Elian Ibarra";
+            sueldo = 3500.55;
+            casado = false;
+
+            Console.WriteLine("Hola, Mi nombre es: " + nombre + " Y tengo " + edad + " años" + " Y mi sueldo actual es de " + sueldo + " dólares");
+            Console.WriteLine("");
+            Console.WriteLine("Me encuentro casado?: " + casado);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c#/Cursos/Udemy - Nivel Básico C#/Solución_Nivel_Básico_CSharp/PJ_HolaMundo/Program.cs
@@ -0,0 +1,26 @@
+﻿// Código predeterminado de Visual Studio - Uso de bibliotecas
+using System; // Importación de biblioteca para el uso de la consola - Console
+
+namespace PJ_HolaMundo
+{
+    internal class Program
+    {
+        // Entorno de trabajo estático
+        static void Main(string[] args)
+        {
+
+            // Escribir texto con salto de línea
+            Console.WriteLine("La suma de 5 + 3 es igual a = ");
+            Console.WriteLine(11/2);
+            Console.WriteLine(11 % 2);
+            Console.WriteLine("Imprimiendo texto en C# usando la Consola");
+
+            // Escribir texto sin salto de línea
+            Console.Write("Escribir sin salto de línea");
+            Console.Write("Linea2");
+
+            // Va a esperar a que se presione una tecla para cerrar la ventana
+            Console.ReadKey();
+        }
+    }
+}
